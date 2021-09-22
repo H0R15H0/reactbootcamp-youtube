d6875a9a71026d8f085fd4b86a8164352feb864e
H0R15H0 <shohori13@gmail.com>
2021-09-23 06:10:20 +0900

    Add: VideoHorizontalCard

---
+++ src/pages/Watch/VideoPlayerCard/style.ts
@@ -0,0 +1,16 @@
+// src/pages/Watch/VideoPlayerCard/style.ts
+
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles({
+  paddingHorizontalLess: {
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
+  transparent: {
+    backgroundColor: "transparent",
+  },
+  descPadding: {
+    paddingLeft: 56
+  },
+});
