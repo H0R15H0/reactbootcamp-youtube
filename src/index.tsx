55bcc8ad76daee9b2b3ec891b249187948a62fb1
H0R15H0 <shohori13@gmail.com>
2021-09-23 05:14:14 +0900

    Add: material UI settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,32 @@
+// src/index.tsx
+
 import React from "react";
 import ReactDOM from "react-dom";
-// BrowserRouterをインポート
 import { BrowserRouter } from "react-router-dom";
 import { RootRouter } from "./Route";
+
+// Material-UIの設定類をインポート
+import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
+
+// Material-UIの「テーマ」を作成する。
+// Material-UIのテーマ（色など）をカスタマイズする際には、createThemeの引数にカスタマイズ項目を渡す。
+// 今回は何もカスタマイズしないので、何も指定していない。
+const theme = createTheme();
 
 ReactDOM.render(
   <React.StrictMode>
     {/*
-      この位置に<BrowserRouter>を挿入する
-      これにより、react-router-domが初期化され、アプリケーションでreact-router-domが使用できるようになる
+      Material-UI用を初期化し、アプリケーション全体でMaterial-UIを使用できるようにする
     */}
-    <BrowserRouter>
-      <RootRouter />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        {/*
+          ブラウザの違いを吸収し、どのデバイスでも同じように表示する用のCSSを使用する
+        */}
+        <CssBaseline />
+        <RootRouter />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
