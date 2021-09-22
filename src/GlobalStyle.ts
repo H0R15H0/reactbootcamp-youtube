157322de890674f8072cef9aeb51396d811d9284
H0R15H0 <shohori13@gmail.com>
2021-09-23 05:46:57 +0900

    Add: GlobalStyle

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,25 @@
+// src/GlobalStyle.tsを作成
+// GlobalStyle.tsのコード
+import { withStyles } from "@material-ui/styles";
+
+export default withStyles({
+  // アプリケーションの全体の高さを全てに指定
+  "@global": {
+    html: {
+      width: "100%",
+      height: "100%",
+    },
+    body: {
+      width: "100%",
+      height: "100%",
+    },
+    "#root": {
+      width: "100%",
+      height: "100%",
+    },
+
+    // おまけで追加
+    img: { display: "block", maxWidth: "100%" },
+  },
+})(() => null);
+
